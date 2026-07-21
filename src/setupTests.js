6aad3ec68ea0325feec059d391cc142f3e2881db
Julie <j49411@gmail.com>
2026-07-21 22:44:08 +0100

    Mock dialog

--- src/setupTests.js
+++ src/setupTests.js
@@ -2,7 +2,18 @@ import "@testing-library/jest-dom";
 import { beforeAll, afterEach, afterAll } from "vitest";
 import { server } from "./mocks/server";
 
-beforeAll(() => server.listen({ onUnhandledRequest: "error" }));
+beforeAll(() => {
+  server.listen({ onUnhandledRequest: "error" });
+  HTMLDialogElement.prototype.showModal = function () {
+    this.open = true;
+  };
+
+  HTMLDialogElement.prototype.close = function () {
+    this.open = false;
+    // Dispatching a close event ensures any 'onClose' listeners in your React component trigger correctly
+    this.dispatchEvent(new Event("close"));
+  };
+});
 
 afterEach(() => server.resetHandlers());
 
